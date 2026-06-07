aab533825661a5a4422d8a48aea0e6d656c2ad8f
VuToV-Mykola <vutov_nikola@icloud.com>
2026-06-07 21:55:34 +0200

    add HW 03-validation

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -2,8 +2,37 @@ import createHttpError from 'http-errors';
 import Note from '../models/note.js';
 
 export async function getAllNotes(req, res) {
-  const notes = await Note.find();
-  res.status(200).json(notes);
+  const { page = 1, perPage = 10, tag, search } = req.query;
+
+  const notesQuery = Note.find();
+
+  if (tag) {
+    notesQuery.where('tag').equals(tag);
+  }
+
+  if (search) {
+    notesQuery.where({
+      $or: [
+        { title: { $regex: search, $options: 'i' } },
+        { content: { $regex: search, $options: 'i' } },
+      ],
+    });
+  }
+
+  const totalNotes = await Note.countDocuments(notesQuery.getFilter());
+
+  const skip = (page - 1) * perPage;
+  const notes = await notesQuery.skip(skip).limit(perPage);
+
+  const totalPages = Math.ceil(totalNotes / perPage);
+
+  res.status(200).json({
+    page,
+    perPage,
+    totalNotes,
+    totalPages,
+    notes,
+  });
 }
 
 export async function getNoteById(req, res) {
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { errors } from 'celebrate';
 import { connectMongoDB } from './db/connectMongoDB.js';
 import notesRoutes from './routes/notesRoutes.js';
 import { logger } from './middleware/logger.js';
@@ -22,6 +23,7 @@ app.use(cors());
 app.use(notesRoutes);
 
 app.use(notFoundHandler);
+app.use(errors());
 app.use(errorHandler);
 
 app.listen(PORT, () => {
